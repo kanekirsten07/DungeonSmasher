46cff67e9beb4f378adc31eb580b54dc99f150f5
Kirsten Kane <kanekirsten07@gmail.com>
2015-11-12 01:23:55 -0800

    Adding mob generator prefab
    this commit adds a mob generator prefab which will determine based on a
    1 in 5 chance whether to add a demon to the room.
    This can be expanded upon later to include more enemies.

---
+++ Assets/Scripts/MobGenerator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MobGenerator : MonoBehaviour {
+
+    public GameObject demon;
+	// Use this for initialization
+	void Start () {
+	
+         int spawnSeed = Random.Range(0, 5);
+        GameObject spawn = new GameObject();
+        switch (spawnSeed)
+        {
+            case 0:
+                spawn = Instantiate(demon, transform.position, transform.rotation) as GameObject;
+                Debug.Log("Demon Generated");
+                break;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
